b3adf5aa88187b6a9f6d210511be076391b573b0
Peekaey <dallasjt31@gmail.com>
2024-04-01 15:48:39 +1100

    added support for custom polling time and cleanup code

--- src/commands.rs
+++ src/commands.rs
@@ -6,12 +6,11 @@ use std::time::{Duration, Instant};
 use chrono;
 use chrono::Local;
 use notify::{RecommendedWatcher, RecursiveMode, Watcher};
-use notify_debouncer_full::{notify::*, new_debouncer, DebounceEventResult, Debouncer, new_debouncer_opt};
-
+use notify_debouncer_full::{notify::*};
 
 pub fn copy_file(original_file_path: &String, save_path: &String) -> bool {
     return if fs::copy(&original_file_path, &save_path).is_ok() {
-        println!("File copied successfully");
+        println!("File copied successfully, saved to: {}", save_path);
         true
     } else {
         println!("Failed to copy file");
@@ -19,17 +18,72 @@ pub fn copy_file(original_file_path: &String, save_path: &String) -> bool {
     }
 }
 
-pub fn create_file_name(file_path: &String, backup_save_path: String) -> String {
+pub fn create_file_name(file_path: &String, backup_save_path: &String) -> String {
     let full_file_name = file_path.split("/").last().unwrap();
     let file_name_parts: Vec<_> = full_file_name.split(".").collect();
     let file_name = file_name_parts[0];
     let file_extension = file_name_parts[1];
     let new_file_name = format!("{} {} {} {}", file_name, Local::now().format("%d-%m-%Y"), Local::now().format("%H-%M-%S"), file_extension);
-    let backup_save_path = backup_save_path + "/" + new_file_name.as_str();
-    println!("New Save Path: {}", backup_save_path);
-    return backup_save_path;
+    let new_backup_save_path = backup_save_path.to_owned() + "/" + new_file_name.as_str();
+    println!("New Save Path: {}", new_backup_save_path);
+    return new_backup_save_path;
 }
 
+// Automatic Polling with updating next polling time
+pub fn watch_poller<P: AsRef<Path>>(path: P, string_save_path: String, string_file_path: String, polling_rate: u64) -> notify::Result<()> {
+    let (tx, rx) = channel();
+    // use the PollWatcher and disable automatic polling
+    let mut watcher = PollWatcher::new(tx, notify::Config::default().with_manual_polling())?;
+
+    // Add a path to be watched. All files and directories at that path and
+    // below will be monitored for changes.
+    watcher.watch(path.as_ref(), RecursiveMode::NonRecursive)?;
+
+    // run event receiver on a different thread
+    std::thread::spawn(move || {
+        for res in rx {
+            match res {
+                Ok(event) => {
+                    let temp_save_path = string_save_path.clone();
+                    if let new_string_save_path = create_file_name(&string_file_path, &temp_save_path) {
+                        if copy_file(&string_file_path, &new_string_save_path) {
+                            println!("File Event Change Type: {:?}", event);
+                        } else {
+                            println!("File had no changes");
+                        }
+                    } 
+                }
+                Err(e) => {
+                    println!("Error while watching file: {:?}", e)
+                }
+            }
+        }
+    });
+
+    // Set up a timer to trigger polling every 60 seconds
+    let mut last_poll_time = Instant::now();
+    let poll_interval = Duration::from_secs(polling_rate);
+
+    loop {
+        let elapsed = last_poll_time.elapsed();
+        if elapsed >= poll_interval {
+            println!("Polling for changes...");
+            watcher.poll().unwrap();
+            last_poll_time = Instant::now();
+        } else {
+            let remaining_time = poll_interval - elapsed;
+            print!("Next poll in {} seconds", remaining_time.as_secs());
+            // Flush the print buffer to ensure the message is displayed immediately
+            let _ = std::io::stdout().flush();
+            // Use carriage return to overwrite the previous line
+            print!("\r");
+            // Sleep for a short duration to prevent excessive CPU usage
+            std::thread::sleep(Duration::from_millis(500));
+        }
+    }
+}
+
+// Below functions are just examples not currently in use
 // Implemented as per https://github.com/notify-rs/notify/blob/18df78efff79a6eb7e1305280957c4b3ae43e1df/examples/monitor_raw.rs
 pub fn watch<P: AsRef<Path>>(path: P) -> notify::Result<()> {
     let (tx, rx) = std::sync::mpsc::channel();
@@ -80,49 +134,5 @@ pub  fn manual_watch_poller<P: AsRef<Path>>(path: P) -> notify::Result<()> {
         println!("polling..");
         // manually poll for changes, received by the spawned thread
         watcher.poll().unwrap();
-    }
-}
-
-
-// Automatic Polling with updating Next polling time
-pub fn watch_poller<P: AsRef<Path>>(path: P) -> notify::Result<()> {
-    let (tx, rx) = channel();
-    // use the PollWatcher and disable automatic polling
-    let mut watcher = PollWatcher::new(tx, notify::Config::default().with_manual_polling())?;
-
-    // Add a path to be watched. All files and directories at that path and
-    // below will be monitored for changes.
-    watcher.watch(path.as_ref(), RecursiveMode::Recursive)?;
-
-    // run event receiver on a different thread, we want this one for user input
-    std::thread::spawn(move || {
-        for res in rx {
-            match res {
-                Ok(event) => println!("changed: {:?}", event),
-                Err(e) => println!("watch error: {:?}", e),
-            }
-        }
-    });
-
-    // Set up a timer to trigger polling every 60 seconds
-    let mut last_poll_time = Instant::now();
-    let poll_interval = Duration::from_secs(60);
-
-    loop {
-        let elapsed = last_poll_time.elapsed();
-        if elapsed >= poll_interval {
-            println!("Polling for changes...");
-            watcher.poll().unwrap();
-            last_poll_time = Instant::now();
-        } else {
-            let remaining_time = poll_interval - elapsed;
-            print!("Next poll in {} seconds", remaining_time.as_secs());
-            // Flush the print buffer to ensure the message is displayed immediately
-            let _ = std::io::stdout().flush();
-            // Use carriage return to overwrite the previous line
-            print!("\r");
-            // Sleep for a short duration to prevent excessive CPU usage
-            std::thread::sleep(Duration::from_millis(500));
-        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
 mod commands;
 
-use std::{fs, io, time};
-use std::path::Path;
-use std::time::Duration;
-use std::sync::mpsc::channel;
-use notify::{Watcher, RecursiveMode, PollWatcher, Event, event};
-
-
+use std::{fs, io};
+use notify::{Watcher};
 
 fn main() {
     let mut file_exists = false;
@@ -14,6 +9,10 @@ fn main() {
     
     let mut path_exists = false;
     let mut backup_save_path = String::new();
+    
+    let mut polling_rate_valid = false;
+    let mut string_polling_rate = String::new();
+    let mut int_polling_rate = 0;
     
     while file_exists == false {
         println!("Please enter the full filepath of the file you wish to listen");
@@ -24,14 +23,12 @@ fn main() {
         original_file_path = original_file_path.trim().to_string();
         
         if fs::metadata(&original_file_path).is_ok() {
-            println!("File exists");
+            println!("File existence validated");
             file_exists = true;
         } else {
-            println!("File does not exist. Please enter a valid file path");
-            println!("Current Value of Filepath: {}", original_file_path );
+            println!("File not found. Please enter a valid file path");
         }
     }
-    
     
     while path_exists == false {
         println!("Please enter the full directory where you wish to save copies of the file");
@@ -42,31 +39,46 @@ fn main() {
         backup_save_path = backup_save_path.trim().to_string();
         
         if fs::metadata(&backup_save_path).is_ok() {
-            println!("Path exists");
+            println!("Path existence validated");
             path_exists = true;
         } else {
             println!("Path does not exist. Please enter a valid path");
-            println!("Current Value of Path: {}", backup_save_path );
         }
     }
     
-    // Manipulating the file name
-    let save_path = commands::create_file_name(&original_file_path, backup_save_path);
+    while polling_rate_valid == false {
+        println!("Please enter the polling rate in seconds (Recommended 60)");
+        string_polling_rate.clear();
+        io::stdin().read_line(&mut  string_polling_rate).expect("Failed to read line");
+        
+        int_polling_rate = match string_polling_rate.trim().parse() {
+            Ok(num) => {
+                println!("Valid polling Rate");
+                polling_rate_valid = true;
+                num
+            }
+            Err(e) => {
+                println!("Invalid input, Please enter a valid number");
+                continue;
+            }
+        };
+    }
+    
+    // Creating backup filename and path
+    let save_path = commands::create_file_name(&original_file_path, &backup_save_path);
     
     //Saving File
     commands::copy_file( &original_file_path, &save_path);
     
+    //Handling off initial values
+    let string_save_path = backup_save_path.clone();
+    let string_file_path = original_file_path.clone();
     
-    
-    
-    //Creating a watcher to listen for changes
+    //Creating watcher path to listen for changes
     let watcher_path = original_file_path.clone();
     println!("Watching Path: {}", watcher_path);
     
-    if let Err(e) = commands::watch_poller(watcher_path) {
+    if let Err(e) = commands::watch_poller(watcher_path, string_save_path, string_file_path, int_polling_rate) {
         println!("watch error: {:?}", e);
     }
-    
-
-    
 }
